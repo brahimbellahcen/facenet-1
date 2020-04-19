5e7072ba0f88b7145b0fe27e8c50519c4c50ec48
hustducnv <hust.ducnv@gmail.com>
2020-04-19 20:58:04 +0700

    update verification_file function

--- face_verification.py
+++ face_verification.py
@@ -14,34 +14,7 @@ def verification_file(file1, file2, model=None, plot=False, thresh=0.5):
     face1 = extract_face(file1)
     face2 = extract_face(file2)
 
-    #extract feature
-    face1_emb = get_embedding(model, face1)
-    face2_emb = get_embedding(model, face2)
-
-    #matching
-    score = cosine(face1_emb, face2_emb)
-    title = None
-    result = 0 #not match
-    if score <= thresh:
-        title = 'Match! Score = %.3f' % (score)
-        result = 1
-    else:
-        title = 'NOT match! Score = %.3f' % (score)
-        result = 0
-    
-    if plot:
-        plt.subplot(1, 2, 1)
-        plt.axis('off')
-        plt.imshow(face1)
-        plt.subplot(1, 2, 2)
-        plt.axis('off')
-        plt.imshow(face2)
-        plt.suptitle(title)
-        plt.show()
-    else:
-        print(title)
-
-    return result
+    return verification_facepixels(face1, face2, model=model, plot=plot, thresh=thresh)
 
 
 # input is face_pixels
