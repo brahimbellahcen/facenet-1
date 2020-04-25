614ca2a18112766fdd44c628e30fa20dc94fd6b5
hustducnv <hust.ducnv@gmail.com>
2020-04-26 00:19:49 +0700

    trainsize 20

--- src/mainVNdata.py
+++ src/mainVNdata.py
@@ -110,12 +110,12 @@ def main():
 
     # convert_emb(data_dir + 'detected_X_y.npz', data_dir +'embeddings_X_y.npz', facenet)
 
-    split_data(data_dir +'embeddings_X_y.npz', 15, data_dir + 'embeddings_Xtr_ytr_Xt_yt_15_pass.npz')
+    split_data(data_dir +'embeddings_X_y.npz', 20, data_dir + 'embeddings_Xtr_ytr_Xt_yt_20_pass.npz')
     
 
     train_SVM(
-        data_dir + 'embeddings_Xtr_ytr_Xt_yt_15_pass.npz',
-        CONSTANTS.root + '/model/SVM_FR_VNCeleb_train5_15_pass.joblib')
+        data_dir + 'embeddings_Xtr_ytr_Xt_yt_20_pass.npz',
+        CONSTANTS.root + '/model/SVM_FR_VNCeleb_train5_20_pass.joblib')
         
 
 main()
